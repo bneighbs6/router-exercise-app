caccc9156c3eb24ed1180d6c0bdc7ab99e28a7a7
Brett <brett_neighbors@yahoo.com>
2024-01-21 21:28:48 -0700

    End of lesson code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link, useLocation } from "react-router-dom";
+import NoMatch from './NoMatch';
+
+function Home() {
+ return <p>Home</p>;
+}
+
+function About() {
+  return <p>About</p>;
+}
 
 function App() {
   return (
     <Router>
-      <div className='App'></div>
+      <div className='App'>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
