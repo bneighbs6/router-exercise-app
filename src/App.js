20d45082e2fd63f7382988074e645bc550709573
Brett <brett_neighbors@yahoo.com>
2024-01-22 20:52:51 -0700

    Starter code for lesson Modifying the History

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
 import { BrowserRouter as Router, Route, Switch, Link, useLocation } from "react-router-dom";
+
+import Home from "./Home"
+import About from "./About"
 import NoMatch from './NoMatch';
+import UserProfile from "./UserProfile";
 
-function Home() {
- return <p>Home</p>;
-}
 
-function About() {
-  return <p>About</p>;
-}
+
+
 
 function App() {
   return (
     <Router>
-      <div className='App'>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
         <Switch>
+
           <Route exact path="/">
             <Home />
           </Route>
+
           <Route path="/about">
             <About />
+          </Route>
+
+          <Route path="/users/:userId">
+            <UserProfile />
           </Route>
 
           <Route>
             <NoMatch />
           </Route>
+
         </Switch>
-      </div>
     </Router>
   );
 }
